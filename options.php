9f810d4c96a2d5110747b92ba297ffb7ff5392cf
llevieux <llevieux@lucaslevieux.com>
2015-10-19 20:37:46 -0700

    Initial commit

---
+++ options.php
@@ -0,0 +1,12 @@
+<?php
+//this file will be require_once'd during the options page.
+echo "hi";
+?>
+<div class="wrap">
+<form method="post" action="options.php"> 
+<h2>Meet The Technicians</h2>
+
+
+<?php submit_button(); ?>
+</form>
+</div>
