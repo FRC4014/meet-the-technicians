dc44c6d39aff7b4abeb4fe7e98a996ed51552e44
llevieux <llevieux@lucaslevieux.com>
2015-10-25 00:32:33 -0700

    a start on displaying entries

--- page.php
+++ page.php
@@ -1,5 +1,19 @@
 <?php //file to output to the page using $the_content.  Included in meet-the-technicians.php.
-$the_content = <<<'EOT'
-   
-EOT;
+global $wpdb;
+$tablename = $wpdb->prefix . "meettechnicians";
+$technicians = $wpdb->get_results( "SELECT * FROM $tablename ORDER BY ID", OBJECT );
+$the_content = "";
+shuffle($technicians); //randomize order
+
+foreach($technicians as $person){
+	$the_content .=  '<div class="mt_person">' . $person->name . '</div>';
+	foreach (array(name, grade, years, title, pic, description, quote, hobbies) as $attribute){
+		$the_content .=  '<div id="mt_label_">' . $attribute . '</div>';
+		
+		$the_content .=  '<div id="' . 'mt_' . $person->id . "_" . $attribute . '">' .
+					$person->$attribute . 
+					'</div>' . "\n";
+		
+		}
+	}
 ?>
