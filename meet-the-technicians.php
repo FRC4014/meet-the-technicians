8e762d3b7cc5dcd14d12b606cdc06a20c74bc060
llevieux <llevieux@lucaslevieux.com>
2015-10-26 01:13:10 -0700

    changed table version to allow database update for previous commit(URL length)

--- meet-the-technicians.php
+++ meet-the-technicians.php
@@ -10,7 +10,7 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-$tableversion = "19"; //arbitrary, change when table structure changes
+$tableversion = "20"; //arbitrary, change when table structure changes
 function meet_technicians($the_content) {
 	if (get_the_title() == "Meet the Technicians"){
 		require_once(ABSPATH . "wp-content/plugins/meet-the-technicians/page.php"); //seperate file for page code
