6697abb2e34965680187f67414bd2a77083afa65
llevieux <llevieux@lucaslevieux.com>
2015-10-26 13:38:04 -0700

    add PHPDoc

--- meet-the-technicians.php
+++ meet-the-technicians.php
@@ -11,14 +11,29 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 $tableversion = "20"; //arbitrary, change when table structure changes
+
+/**
+ * Filters the post_content.  Replaces the_content with Meet the Technicians
+ * content if the title is correct.  Includes page.php.
+ * 
+ * @param string $the_content the input from the_content, insde the_loop
+ * @return string $the_content filtered input, technicians content if applicable
+ */
 function meet_technicians($the_content) {
 	if (get_the_title() == "Meet the Technicians"){
 		require_once(ABSPATH . "wp-content/plugins/meet-the-technicians/page.php"); //seperate file for page code
 		}
   	return $the_content;
 	}
+	
+/**
+ * Creates or updates SQL table.
+ * 
+ * @global class $wpdb wordpress database manager
+ * @global string $tableversion arbitrary version set in this fule
+ */
 function createTechniciansTable() {
-	global $wpdb, $tableversion, $update_result;
+	global $wpdb, $tableversion;
 	$charset_collate = $wpdb->get_charset_collate();
 	$table_name = $wpdb->prefix . "meettechnicians"; 
 	
@@ -35,7 +50,8 @@ function createTechniciansTable() {
 		PRIMARY KEY (id)
 		) $charset_collate;";
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	$update_result = dbDelta( $sql );
+	
+	dbDelta( $sql );
 	update_option( 'meettechniciansversion', $tableversion );
 	add_action( 'admin_notices', 'table_updated_notice' ); //display a notice
 	}
@@ -49,6 +65,12 @@ function notsaved_notice() {
 	admin_notice("Not saved", "error");
 	}
 
+/**
+ * Echos the notice in the admin area.  
+ * 
+ * @param string $notice message to send to user
+ * @param string $class type of notice. "updated", "error"
+ */
 function admin_notice ($notice, $class = "updated"){
 	?>
     <div class="<?= $class ?>">
@@ -56,24 +78,36 @@ function admin_notice ($notice, $class = "updated"){
     </div>
     <?php
 	}
-	
+
+/**
+ * Adds options to settings menu. add_action'd at 'admin_menu'
+ */
 function meet_technicians_menu() {
 	add_options_page( "Meet The Technicians", "Meet The Technicians", 'manage_options', "meet-the-technicians.php", "meet_technicians_options"); 
 	}
+	
+/**
+ * Code for options page, called on associated page
+ */
 function meet_technicians_options() {
-	//code for options page goes here
 	require_once(ABSPATH . "wp-content/plugins/meet-the-technicians/options.php");
 	}
 
+/**
+ * Registers and enqueues options.css on appropriate admin page.
+ * @param string $hook hook data passed by add_action
+ */
 function mt_enqueue_admin_style($hook) {
     if ( 'settings_page_meet-the-technicians' != $hook ) {
         return;
 		}
     wp_register_style( 'mt_admin_style', plugin_dir_url( __FILE__ ) . 'options.css' );
 	wp_enqueue_style( 'mt_admin_style' );
-
 	}
 	
+/**
+ * Registers and enqueues page.css on appropriate frontend page.
+ */
 function mt_enqueue_page_style() {
     if (get_the_title() != "Meet the Technicians") {
         return;
@@ -81,7 +115,11 @@ function mt_enqueue_page_style() {
     wp_register_style( 'mt_page_style', plugin_dir_url( __FILE__ ) . 'page.css' );
 	wp_enqueue_style( 'mt_page_style' );
 	}
-	
+
+/**
+ * Registers and enqueues script.js on appropriate admin and frontend pages.
+ * @param string $hook hook data passed by add_action
+ */
 function mt_enqueue_script($hook) {
 	if (get_the_title() != "Meet the Technicians" and $hook != 'settings_page_meet-the-technicians') {
         return;
