27656293f25d62edb3af126509ba7d543c27499d
llevieux <llevieux@lucaslevieux.com>
2015-10-25 20:29:49 -0700

    implement field names, lengths, size, isoptional

--- options.php
+++ options.php
@@ -27,18 +27,35 @@ if (isset($_POST["save"])){ //save POST data to database
 $technicians = $wpdb->get_results( "SELECT * FROM $tablename ORDER BY id ASC", OBJECT );
 foreach($technicians as $person){
 	echo '<fieldset class="mt_person"><legend>' . $person->name . '</legend>' . "\n";
-	foreach (array(name, grade, years, title, pic, description, quote, hobbies) as $attribute){
+	$fields = array( //array(display label, max length (see database), is required, width, size)
+		name => array("Name", 30, true, 30),  
+		grade => array("Grade", 2, true, 2), 
+		years => array("Years on the Team", 2, true, 2), 
+		title => array("Title/Responsibilities", 60, true, 30), 
+		pic => array("URL to Picture", 55, true, 30), 
+		description => array("Description", 200, false), 
+		quote => array("Quote", 50, false, 30), 
+		hobbies => array("Hobbies", 50, false, 30)
+			);
+	foreach ($fields as $attribute => $info){
 		$id = 'mt_' . $person->id . "_" . $attribute;
-		echo '<label class="mt_label">' . $attribute;
+		echo '<label class="mt_label">' . $info[0];
+		if (!$info[2]) { //isn't required field
+			echo ' <span class="mt_optional">optional</span>';
+			}
 		if ($attribute == "description"){ //for longer fields
 			echo '<textarea name="' . $id . '">';
 			echo $person->$attribute;
 			echo '</textarea>'  . '</label>'. "\n";
 			}
 		else {
+			if ($attribute == "grade" or $attribute == "years") $size = 2;
+			else $size = 30;
 			echo '<input type="text" name="' . $id . 
 					'" value="' . $person->$attribute . 
-					'" />' . '</label>' . "\n";
+					'" maxlength = "' . $info[1] .
+					'" size = "' . $size .
+					'"/>' . '</label>' . "\n";
 			}
 		}
 	echo '</fieldset>';
