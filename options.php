836299db4a1c923a2f9ab9f6130c819488b3e96f
llevieux <llevieux@lucaslevieux.com>
2015-10-26 18:44:50 -0700

    allow for empty add person, sleek new css and javascript for add person

--- options.php
+++ options.php
@@ -17,45 +17,85 @@ if (isset($_POST["submit"])){ //save POST data to database
 				(strpos($sliver, "_") + 1) //before second _, after first _
 				);
 		$attribute = substr($sliver, 
-				strpos($sliver, $mt_id) + 2 //after _, after id
+				strpos($sliver, $mt_id) + strlen($mt_id) + 1 //after _, after id
 				);
 		$data_array[$mt_id][$attribute] = $data;
 		
-		echo "id: $mt_id, attribute: $attribute, data: $data<br>";
+		//echo "id: $mt_id, attribute: $attribute, data: $data<br>";
 		}
 	$new_array = array();
 	foreach ($data_array as $db_id => $data){
+		if ($data[name] == "" and 
+					$data[grade] == "" and
+					$data[years] == "" and
+					$data[title] == "" and
+					$data[pic] == ""){
+				continue; //none of new is filled out
+				}
+		if ($data[name] == "" or 
+					$data[grade] == "" or
+					$data[years] == "" or
+					$data[title] == "" or
+					$data[pic] == ""){
+				$this->adminNotice("A required field is empty", "error");
+				continue;
+				}
+		if ($db_id == "new"){
+			$db_id = $last_id + 1; //should be in order of id, so last one will be greatest
+			print_r($data);
+			$succeed = $wpdb -> insert($tablename, 
+					array_merge(array("id" => $db_id), $data), 
+					array(
+					'%d', //id
+					'%s', //name
+					'%d', //grade
+					'%d', //years
+					'%s', //title
+					'%s', //pic
+					'%s', //description
+					'%s', //quote
+					'%s'  //hobbies
+					));
+			if ($succeed !== false){ //can be successful and return 0
+				$this->adminNotice ("New person \"$data[name]\" added!");
+				}
+			else {
+				$this->adminNotice("Not saved", "error");
+				}
+			continue; //don't allow it to add to $new_array
+			}
+		$last_id = $db_id;
 		$new_array[] = array_merge(array("id" => $db_id), $data);
 		}
 	$thingsChanged = 0;
-	//print_r($new_array);
-//	foreach ($new_array as $person => $data){
-//		$differences = array_diff($data, $technicians[$person]);
-//		//print_r($differences);
-//		foreach ($differences as $name => $value){
-//			if (gettype($value) == "string") {$datatype = '%s';}
-//			else {$datatype = '%d';}
-//			//echo "table: $tablename, data: array($name => $value), which: array(\"id\" => $person), datatype: $datatype, whichtype: %d" ;
-//			$succeed = $wpdb -> update($tablename, array($name => $value), array("id" => $data["id"]), $datatype, "%d");
-//			
-//			if ($succeed !== false){ //can be successful and return 0
-//				$thingsChanged += $succeed;
-//				}
-//			else {
-//				$this->adminNotice("Not saved", "error");
-//				break 2;
-//				}
-//			}
-//		if ($thingsChanged > 1){
-//			$this->adminNotice ("Saved. $thingsChanged fields changed!");
-//			}
-//		else if ($thingsChanged === 1){
-//			$this->adminNotice ("Saved. One field changed!");
-//			}
-//		else {
-//			$this->adminNotice ("No changes to save.", "error");
-//			}
-//		}
+	print_r($new_array);
+	foreach ($new_array as $person => $data){
+		$differences = array_diff($data, $technicians[$person]);
+		//print_r($differences);
+		foreach ($differences as $name => $value){
+			if (gettype($value) == "string") {$datatype = '%s';}
+			else {$datatype = '%d';}
+			//echo "table: $tablename, data: array($name => $value), which: array(\"id\" => $person), datatype: $datatype, whichtype: %d" ;
+			$succeed = $wpdb -> update($tablename, array($name => $value), array("id" => $data["id"]), $datatype, "%d");
+			
+			if ($succeed !== false){ //can be successful and return 0
+				$thingsChanged += $succeed;
+				}
+			else {
+				$this->adminNotice("Not saved", "error");
+				break 2;
+				}
+			}
+		}
+	if ($thingsChanged > 1){
+			$this->adminNotice ("Saved. $thingsChanged fields changed!");
+			}
+		else if ($thingsChanged === 1){
+			$this->adminNotice ("Saved. One field changed!");
+			}
+		else {
+			$this->adminNotice ("No changes to save.", "error");
+			}
 	}
 	
 
@@ -82,7 +122,8 @@ array_push($technicians, array(
 foreach($technicians as $person){
 	if ($person[id] == "new"){$legend = "New Person";}
 	else {$legend = $person[name];}
-	echo '<fieldset class="mt_person"><legend>' . $legend . '</legend>' . "\n";
+	echo '<fieldset class="mt_person" id="person_' . $person[id] .
+			'"><legend>' . $legend . '</legend>' . "\n";
 	
 	$fields = array( //array(display label, max length (see database), is required, width, size)
 		name => array("Name", 30, true, 30),  
@@ -132,7 +173,7 @@ foreach($technicians as $person){
 	echo '</fieldset>' . "\n\n";
 	}
 ?>
-
+<a onclick="document.getElementById('person_new').style.display = 'inline-block';document.getElementById('addnew').style.display = 'none';"><fieldset class="mt_person" id="addnew"><div id="plus">+</div><div id="text">add new person</div></fieldset></a>
 <p>
 <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
 <input type="reset" name="reset" id="reset" class="button button-secondary" value="Reset">
