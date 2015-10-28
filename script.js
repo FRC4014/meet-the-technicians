7386b7de5abe8f77d57ef80a6cc3e4bb9f079e6d
llevieux <llevieux@lucaslevieux.com>
2015-10-27 18:15:11 -0700

    Revert "moved javascript to appropriate file"
    This reverts commit ecb73dd8776c8d3c8d3a452446a270eb974441bd.

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ function formSubmit(id) {
         document.getElementById("mt_form").submit();
         }
     }
-function revealNewPerson (){
-    document.getElementById('person_new').style.display = 'inline-block';
-    document.getElementById('addnew').style.display = 'none';
-    }
-
-var body = document.getElementsByTagName("body")[0];
-window.addEventListener("load", redirectPage, false);
-
-function redirectPage(){
-    if (document.getElementById('title').value === 'Meet the Technicians') {
-        window.location = "edit.php?post_type=page&page=meet-the-technicians.php";
-        }
-    }
