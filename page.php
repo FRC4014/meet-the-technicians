edf79f59767717ca943043cad4d47abcc0b4c26b
llevieux <llevieux@lucaslevieux.com>
2016-01-15 21:46:32 -0800

    new DOM structure

--- page.php
+++ page.php
@@ -5,14 +5,15 @@ $the_content = "";
 shuffle($people); //randomize order
 
 foreach($people as $person){
-	$the_content .=  '<div class="mt_person">' . $person[name] . '</div>';
+	$the_content .=  '<div class="mt_person" id="mt_' . $person[id] . '">';
 	foreach (array(name, grade, years, title, pic, description, quote, hobbies) as $attribute){
 		if ($person[$attribute] == "") continue;
-		$the_content .=  '<div id="mt_label_">' . $attribute . '</div>';
-		$the_content .=  '<div id="' . 'mt_' . $person[id] . "_" . $attribute . '">' .
+		$the_content .=  '<div class="mt_label ' . $attribute . '">' . $attribute . '</div>';
+		$the_content .=  '<div class="mt_field mt_' . $attribute . '">' .
 					$person[$attribute] . 
 					'</div>' . "\n";
 		
 		}
+	$the_content .= '</div>';
 	}
 ?>
