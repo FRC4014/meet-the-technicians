81771a6f3a259f1806a031ae3522220fdf62afe3
llevieux <llevieux@lucaslevieux.com>
2016-01-17 23:14:09 -0800

    added background

--- page.php
+++ page.php
@@ -6,8 +6,9 @@ shuffle($people); //randomize order
 
 $the_content .=  '<div id="mt">';
 foreach($people as $person){
-	$the_content .=  '<div class="person" id="' . $person[id] . '">';
-	foreach (array(name, grade, years, title, pic, description, quote, hobbies) as $attribute){
+	$the_content .=  '<div class="person" id="' . $person[id] . '" '
+			. 'style="background-image: url(' . $person[pic] . ');">' . "\n";
+	foreach (array(name, grade, years, title, description, quote, hobbies) as $attribute){
 		if ($person[$attribute] == "") continue;
 		$the_content .=  '<div class="field ' . $attribute . '">' .
 					$person[$attribute] . 
