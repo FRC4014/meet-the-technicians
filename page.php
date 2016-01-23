a248a98e5a0f7ed622dc2f288111267ed51065b9
llevieux <llevieux@lucaslevieux.com>
2016-01-23 14:42:44 -0800

    remove friendly words and replaced with heartless numbers

--- page.php
+++ page.php
@@ -8,23 +8,26 @@ $the_content .=  '<div id="mt">';
 foreach($people as $person){
 	$the_content .=  '<div class="person" id="' . $person[id] . '" '
 			. 'style="background-image: url(' . $person[pic] . ');">' . "\n";
-	foreach (array(name, grade, years, title, description, quote, hobbies) as $attribute){
+	foreach (array(name, title, grade, years, quote, hobbies, description) as $attribute){
 		$label = null;
 		if ($person[$attribute] == "") continue;
-		else if ($attribute == "grade") {
-			if ($person[$attribute]  == 9) $person[$attribute] = "Nineth";
-			if ($person[$attribute]  == 10) $person[$attribute] = "Tenth";
-			if ($person[$attribute]  == 11) $person[$attribute] = "Eleventh";
-			if ($person[$attribute]  == 12) $person[$attribute] = "Twelfth";
-			$person[$attribute] .= " grade";
-			}
-		else if ($attribute == "years") {
-			if ($person[$attribute] == 1)
-				$person[$attribute] = "First year on the team";
-			else
-				$person[$attribute] .= " years on the team";
-			}
-		else if ($attribute == "quote") $label = "Favorite quote:";
+//		else if ($attribute == "grade") {
+//			if ($person[$attribute]  == 9) $person[$attribute] = "Nineth";
+//			if ($person[$attribute]  == 10) $person[$attribute] = "Tenth";
+//			if ($person[$attribute]  == 11) $person[$attribute] = "Eleventh";
+//			if ($person[$attribute]  == 12) $person[$attribute] = "Twelfth";
+//			$person[$attribute] .= " grade";
+//			}
+//		else if ($attribute == "years") {
+//			if ($person[$attribute] == 1)
+//				$person[$attribute] = "First year on the team";
+//			else
+//				$person[$attribute] .= " years on the team";
+//			}
+		else if ($attribute == "grade") $label = "Grade:";
+		else if ($attribute == "years") $label = "Years on the team:";
+		else if ($attribute == "title") $label = "Title/Responsibilities:";
+		else if ($attribute == "quote") $label = "Favorite Quote:";
 		
 		if (isset($label))
 			$the_content .=  '<div class="label ' . $attribute . '">' .
